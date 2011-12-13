9a9fa5d0db103d7604bfef3253d66194796d1708
Matthew Halupka <matt@ubuntu.(none)>
2011-12-13 15:44:44 -0800

    Changed to datainputstream, and did some code housekeeping

--- CannonClient.java
+++ CannonClient.java
@@ -11,180 +11,183 @@ import utils.ToolKit;
 import peers.Peer;
 
 public class CannonClient {
-  
-  public static byte[] PEER_ID = new byte[20];
-  public static byte[] INFO_HASH = new byte[20];
-  public static TorrentInfo TORRENT_INFO;
-	
+
+	public static byte[] PEER_ID = new byte[20];
+	public static byte[] INFO_HASH = new byte[20];
+	public static TorrentInfo TORRENT_INFO;
+
 	public static void main(String[] args) {
-    
-    String torrentFile = args[0];
-    String savedFile = args[1];
-    setPeerId();
 
-    //set up the torrent info
-    try{
-      TORRENT_INFO = new TorrentInfo(readTorrent(torrentFile));
-      TORRENT_INFO.info_hash.get(INFO_HASH, 0, INFO_HASH.length);
-    } catch (Exception e){
-    	e.printStackTrace();
-      System.out.println("Torrent could not be loaded.");
-    }
-    
-    //query tracker
-    //TODO: write a loop that cycles through ports
-    for (int i=6881; i<=6889;){
-      try{
-        String url = constructQuery(i, 0, 0, TORRENT_INFO.file_length); 
-        byte[] response = getURL(url);
-        ArrayList<Peer> peerList = getPeers(response);
-        for(Peer peer : peerList){
-          if (peer.isValid()){
-            System.out.println("Peer Found");
-            peer.createSocket(peer.ip_, peer.port_);
-            peer.establishStreams();
-            peer.sendHandshake(PEER_ID, INFO_HASH);
-            if(peer.receiveHandshake(INFO_HASH)){
+		String torrentFile = args[0];
+		String savedFile = args[1];
+		setPeerId();
 
-             //TODO: do we want what they have?
-             peer.sendInterested();
+		//set up the torrent info
+		try{
+			TORRENT_INFO = new TorrentInfo(readTorrent(torrentFile));
+			TORRENT_INFO.info_hash.get(INFO_HASH, 0, INFO_HASH.length);
+		} catch (Exception e){
+			e.printStackTrace();
+			System.out.println("Torrent could not be loaded.");
+		}
 
-             //listen for the unchoke message
-             while(true){ if(peer.listenForUnchoke()){ break; }}
-             
-             //start downloading!
-             peer.sendRequest(0, 0, 16384);
-             while(true){ peer.listenForPiece(); }
-            }
-          }
-        }
-        
-        break;
-      } catch (Exception e){
-      	e.printStackTrace();
-        System.out.println("Port " + i + " failed."); 
-        i++;
-      }
-    }
+		//query tracker
+		//TODO: write a loop that cycles through ports
+		for (int i=6881; i<=6889;){
+			try{
+				String url = constructQuery(i, 0, 0, TORRENT_INFO.file_length); 
+				byte[] response = getURL(url);
+				ArrayList<Peer> peerList = getPeers(response);
+				for(Peer peer : peerList){
+					if (peer.isValid()){
+						System.out.println("Peer Found");
+						peer.createSocket(peer.ip_, peer.port_);
+						peer.establishStreams();
+						peer.sendHandshake(PEER_ID, INFO_HASH);
+						if(peer.receiveHandshake(INFO_HASH)){
+
+							//TODO: do we want what they have?
+							//Sends interested message
+							int temp1 = 1;
+							byte temp2 = 0x02;
+							peer.sendMessage(temp1, temp2);
+
+							//listen for the unchoke message
+							while(true){ if(peer.listenForUnchoke()){ break; }}
+
+							//start downloading!
+							peer.sendRequest(0, 0, 16384);
+							while(true){ peer.listenForPiece(); }
+						}
+					}
+				}
+
+				break;
+			} catch (Exception e){
+				e.printStackTrace();
+				System.out.println("Port " + i + " failed."); 
+				i++;
+			}
+		}
 	}
-	
+
 	public static ArrayList<Peer> getPeers(byte[] response){
-	  ArrayList<Peer> peerList = new ArrayList<Peer>();
+		ArrayList<Peer> peerList = new ArrayList<Peer>();
 
-	  try{
-      //decode the response and slap it in an array for perusal
-      Object decodedResponse = Bencoder2.decode(response);
-      // ToolKit.print(decodedResponse, 1);
-      
-      Map<ByteBuffer, Object> responseMap = (Map<ByteBuffer, Object>)decodedResponse;
-      Object[] responseArray = responseMap.values().toArray();
-      
-      //the interval returned by the tracker
-      int interval = (Integer)responseArray[0];
-      
-      //the list of peers returned by the tracker
-      Object peers = responseArray[1];
-      
-      ArrayList<Object> peerArray = (ArrayList<Object>)peers;
-      
-      //iterate through the list
-      for (Object peer : peerArray){
-        String ip_ = "";
-        String peer_id_ = "";
-        int port_ = 0;
-        
-        Map<ByteBuffer, Object> peerMap = (Map<ByteBuffer, Object>)peer;
-        
-        //get all the properties
-        for (Map.Entry<ByteBuffer, Object> entry : peerMap.entrySet()){          
-           String key = Helpers.bufferToString(entry.getKey());
-           Object value = entry.getValue();
-           if (key.compareTo("ip") == 0){
-             ip_ = Helpers.bufferToString((ByteBuffer)value);
-           }
-           if (key.compareTo("peer id") == 0){
-             peer_id_ = Helpers.bufferToString((ByteBuffer)value);
-           }
-           if (key.compareTo("port") == 0){
-             //TODO: this sometimes throws an error, for god knows why: java.nio.HeapByteBuffer cannot be cast to java.lang.Integer
-             port_ = (Integer)value;
-          }
-        }
-        //add the fleshed out peer to the peerList
-        Peer newPeer = new Peer(peer_id_, ip_, port_);
-        peerList.add(newPeer);
-      }
-    } catch (Exception e){
-      System.out.print(e);
-    }
+		try{
+			//decode the response and slap it in an array for perusal
+			Object decodedResponse = Bencoder2.decode(response);
+			// ToolKit.print(decodedResponse, 1);
 
-    return peerList;
+			Map<ByteBuffer, Object> responseMap = (Map<ByteBuffer, Object>)decodedResponse;
+			Object[] responseArray = responseMap.values().toArray();
+
+			//the interval returned by the tracker
+			int interval = (Integer)responseArray[0];
+
+			//the list of peers returned by the tracker
+			Object peers = responseArray[1];
+
+			ArrayList<Object> peerArray = (ArrayList<Object>)peers;
+
+			//iterate through the list
+			for (Object peer : peerArray){
+				String ip_ = "";
+				String peer_id_ = "";
+				int port_ = 0;
+
+				Map<ByteBuffer, Object> peerMap = (Map<ByteBuffer, Object>)peer;
+
+				//get all the properties
+				for (Map.Entry<ByteBuffer, Object> entry : peerMap.entrySet()){          
+					String key = Helpers.bufferToString(entry.getKey());
+					Object value = entry.getValue();
+					if (key.compareTo("ip") == 0){
+						ip_ = Helpers.bufferToString((ByteBuffer)value);
+					}
+					if (key.compareTo("peer id") == 0){
+						peer_id_ = Helpers.bufferToString((ByteBuffer)value);
+					}
+					if (key.compareTo("port") == 0){
+	//TODO: this sometimes throws an error, for god knows why: java.nio.HeapByteBuffer cannot be cast to java.lang.Integer
+						port_ = (Integer)value;
+					}
+				}
+				//add the fleshed out peer to the peerList
+				Peer newPeer = new Peer(peer_id_, ip_, port_);
+				peerList.add(newPeer);
+			}
+		} catch (Exception e){
+			System.out.print(e);
+		}
+
+		return peerList;
 	}
-	
+
 	//construct the url for initially querying the tracker
 	public static String constructQuery(int port, int uploaded, int downloaded, int left){
-	  String url_string = "";
-	  try{
-	    
-      String escaped_hash = Helpers.toURLHex(INFO_HASH);
-      String ip = "128.6.5.130";
-      url_string =  TORRENT_INFO.announce_url.toString()
-                    + "?port=" + port
-                    + "&peer_id=" + PEER_ID
-                    + "&info_hash=" + escaped_hash 
-                    + "&uploaded=" + uploaded
-                    + "&downloaded=" + downloaded
-                    + "&left=" + left
-                    + "&ip=" +  ip;
+		String url_string = "";
+		try{
 
-    } catch (Exception e){
-      System.out.println(e);
-    }
-    
-    return url_string;
+			String escaped_hash = Helpers.toURLHex(INFO_HASH);
+			String ip = "128.6.5.130";
+			url_string =  TORRENT_INFO.announce_url.toString()
+			+ "?port=" + port
+			+ "&peer_id=" + PEER_ID
+			+ "&info_hash=" + escaped_hash 
+			+ "&uploaded=" + uploaded
+			+ "&downloaded=" + downloaded
+			+ "&left=" + left
+			+ "&ip=" +  ip;
+
+		} catch (Exception e){
+			System.out.println(e);
+		}
+
+		return url_string;
 	}
 
 	//read the torrent file into a byte array
 	public static byte[] readTorrent(String torrentFile) {
-    try {
-      RandomAccessFile rFile = new RandomAccessFile(torrentFile,"rw");
-      byte[] fileBytes = new byte[(int)rFile.length()];
-      rFile.read(fileBytes);
-      return fileBytes;
-    } catch (IOException e) {
-        e.printStackTrace();
-        return null;
-    }
-  }
-  
-  //sets the peer id every time the program is run
-  public static void setPeerId(){
-    Random ran = new Random();
-    int rand_id = ran.nextInt(5555555 - 1000000 + 1) + 1000000;
-    String peer_id_string = "GROUP4AREL33t" + rand_id;
-    PEER_ID = peer_id_string.getBytes();
-  }
-  
-  //returns a byte[] consisting of the contents at the given url
-  public static byte[] getURL(String string_url) {
-    ByteArrayOutputStream bais = new ByteArrayOutputStream();
-    InputStream is = null;
-    try {
-      URL url = new URL(string_url);
-      is = url.openStream();
-      byte[] byteChunk = new byte[4096];
-      int n;
+		try {
+			RandomAccessFile rFile = new RandomAccessFile(torrentFile,"rw");
+			byte[] fileBytes = new byte[(int)rFile.length()];
+			rFile.read(fileBytes);
+			return fileBytes;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
-      while ( (n = is.read(byteChunk)) > 0 ) {
-        bais.write(byteChunk, 0, n);
-      }
-      
-      is.close();
-    }
-    catch (IOException e) {
-    }
-    
-    return bais.toByteArray();
-  }
+	//sets the peer id every time the program is run
+	public static void setPeerId(){
+		Random ran = new Random();
+		int rand_id = ran.nextInt(5555555 - 1000000 + 1) + 1000000;
+		String peer_id_string = "GROUP4AREL33t" + rand_id;
+		PEER_ID = peer_id_string.getBytes();
+	}
+
+	//returns a byte[] consisting of the contents at the given url
+	public static byte[] getURL(String string_url) {
+		ByteArrayOutputStream bais = new ByteArrayOutputStream();
+		InputStream is = null;
+		try {
+			URL url = new URL(string_url);
+			is = url.openStream();
+			byte[] byteChunk = new byte[4096];
+			int n;
+
+			while ( (n = is.read(byteChunk)) > 0 ) {
+				bais.write(byteChunk, 0, n);
+			}
+
+			is.close();
+		}
+		catch (IOException e) {
+		}
+
+		return bais.toByteArray();
+	}
 }
 
--- peers/Peer.java
+++ peers/Peer.java
@@ -1,6 +1,7 @@
 package peers;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
@@ -10,32 +11,32 @@ import utils.Helpers;
 import utils.ToolKit;
 
 public class Peer {
-	
+
 	public String peer_id_ = null;
 	public String ip_ = null;
 	public int port_ = 0;
 	public Socket socket_= null;
 	public DataOutputStream to_peer_ = null;
-	public BufferedReader from_peer_ = null;	
-		
+	public DataInputStream from_peer_ = null;	
+
 	public Peer(String _peer_id, String _ip, int _port){
 		this.peer_id_ = _peer_id;
 		this.ip_ = _ip;
 		this.port_ = _port;
 	}
-	
+
 	public void print(){
-	  System.out.println(this.peer_id_);
-	  System.out.println(this.ip_);
-	  System.out.println(this.port_);
-	  System.out.println("");
+		System.out.println(this.peer_id_);
+		System.out.println(this.ip_);
+		System.out.println(this.port_);
+		System.out.println("");
 	}
-	
+
 	public boolean isValid(){
-	  if (this.ip_.equals("128.6.5.130") && this.peer_id_.indexOf("RUBT") != -1){
-	    return true;
-    }
-    return false;
+		if (this.ip_.equals("128.6.5.130") && this.peer_id_.indexOf("RUBT") != -1){
+			return true;
+		}
+		return false;
 	}
 	//Opens sockets given IP and Port
 	public boolean createSocket(String _ip, int _port){
@@ -50,8 +51,7 @@ public class Peer {
 	public boolean establishStreams(){
 		try{
 			to_peer_ = new DataOutputStream(socket_.getOutputStream());
-			from_peer_ = new BufferedReader(new InputStreamReader(
-													socket_.getInputStream()));
+			from_peer_ = new DataInputStream(socket_.getInputStream());
 		}catch(Exception e){
 			e.printStackTrace();
 		}
@@ -84,49 +84,46 @@ public class Peer {
 			e.printStackTrace();
 		}
 	}
-	
+
 	//Recieves incoming handshake from remote peer
 	public boolean receiveHandshake(byte[] _hash){
 		try{
-		  char[] responseHash = new char[20];
-		  byte[] byteResponseHash = new byte[20];
-		  char[] response = new char[68];		  
-      from_peer_.read(response);
-      System.arraycopy(response, 28, responseHash, 0, 20);
-      String c = new String(responseHash);
-      byteResponseHash = c.getBytes();
-      for(int i=0; i<20; i++){
-        if(byteResponseHash[i] != _hash[i]){
-          return false;
-        }
-      }
+			byte[] responseHash = new byte[20];
+			byte[] response = new byte[68];
+			from_peer_.read(response);
+			System.arraycopy(response, 28, responseHash, 0, 20);
+			for(int i=0; i<20; i++){
+				if(responseHash[i]!=_hash[i]){
+					return false;
+				}
+			}
 		}catch(Exception e){
 			e.printStackTrace();
 		}
-    System.out.println("Handshake verified");
-    return true;
+		System.out.println("Handshake verified");
+		return true;
 	}
-	
+
 	public boolean listenForUnchoke(){
-    try{
-      if(from_peer_.read() == 1 && from_peer_.read() == 1){
-        System.out.println("Unchoked");
-        return true;
-      }
-    }catch(Exception e){
-  		e.printStackTrace();
-  	}
-  	return false;
+		try{
+			if(from_peer_.read() == 1 && from_peer_.read() == 1){
+				System.out.println("Unchoked");
+				return true;
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return false;
 	}
-	
+
 	public void listenForPiece(){
-	  try{
-	    System.out.println(from_peer_.read());
-	  }catch(Exception e){
-      e.printStackTrace();
-    }
+		try{
+			System.out.println(from_peer_.read());
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
-	
+
 	public void sendKeepAlive(){
 		byte out_bytes_[] = new byte[4];
 		try{
@@ -135,11 +132,11 @@ public class Peer {
 			e.printStackTrace();
 		}
 	}
-	public void sendChoke(){
+
+	public void sendMessage(int _int,byte _byte){
 		ByteBuffer out_bytes_ = ByteBuffer.allocate(5);
-		out_bytes_.putInt(1);
-		byte temp = 0x00;
-		out_bytes_.put(temp);
+		out_bytes_.putInt(_int);
+		out_bytes_.put(_byte);
 		byte write_out_[] = out_bytes_.array();
 		try{
 			to_peer_.write(write_out_);
@@ -147,55 +144,7 @@ public class Peer {
 			e.printStackTrace();
 		}
 	}
-	public void sendUnchoke(){
-		ByteBuffer out_bytes_ = ByteBuffer.allocate(5);
-		out_bytes_.putInt(1);
-		byte temp = 0x01;
-		out_bytes_.put(temp);
-		byte write_out_[] = out_bytes_.array();
-		try{
-			to_peer_.write(write_out_);
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-	}
-	public void sendInterested(){
-		ByteBuffer out_bytes_ = ByteBuffer.allocate(5);
-		out_bytes_.putInt(1);
-		byte temp = 0x02;
-		out_bytes_.put(temp);
-		byte write_out_[] = out_bytes_.array();
-		try{
-			to_peer_.write(write_out_);
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-	}
-	public void sendUninterested(){
-		ByteBuffer out_bytes_ = ByteBuffer.allocate(5);
-		out_bytes_.putInt(1);
-		byte temp = 0x03;
-		out_bytes_.put(temp);
-		byte write_out_[] = out_bytes_.array();
-		try{
-			to_peer_.write(write_out_);
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-	}
-	public void sendHave(int _index){
-		ByteBuffer out_bytes_ = ByteBuffer.allocate(9);
-		out_bytes_.putInt(5);
-		byte temp = 0x04;
-		out_bytes_.put(temp);
-		out_bytes_.putInt(_index);
-		byte write_out_[] = out_bytes_.array();
-		try{
-			to_peer_.write(write_out_);
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-	}
+
 	public void sendRequest(int _index, int _begin, int _length){
 		ByteBuffer out_bytes_ = ByteBuffer.allocate(17);
 		out_bytes_.putInt(13);
@@ -211,6 +160,8 @@ public class Peer {
 			e.printStackTrace();
 		}
 	}
+
+
 	public void sendPiece(int _index, int _begin, byte[] _block){
 		ByteBuffer out_bytes_ = ByteBuffer.allocate(13+_block.length);
 		out_bytes_.putInt(9+_block.length);
@@ -226,11 +177,11 @@ public class Peer {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void sendBitfield(){
-		
+
 	}
-	
+
 	public String getIP(){
 		return this.ip_;
 	}
@@ -243,6 +194,6 @@ public class Peer {
 	public void setPort(int _port){
 		this.port_ = _port;
 	}
-	
+
 
 }
