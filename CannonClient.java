a31d78dabcd57c2ac51a5cad02abada59668d221
Abe Stanway <abestanway@gmail.com>
2012-02-05 23:45:04 -0500

    string to byte array, packages fucked

--- CannonClient.java
+++ CannonClient.java
@@ -1,6 +1,9 @@
+package cannon;
 
 import java.io.*;
 import java.util.*;
+
+import cannon.*;
 
 public class CannonClient {
 	
@@ -8,21 +11,26 @@ public class CannonClient {
     
     String torrentFile = args[0];
     String savedFile = args[1];
-    readTorrent(torrentFile);
+    byte[] torrentArray = readTorrent(torrentFile);
+    Object decoded = Bencoder2.decode(torrentArray);
 	}
 	
-	public static void readTorrent(String torrentFile){
-	  FileInputStream fin;
-		try{
-		    fin = new FileInputStream (torrentFile);
-		    System.out.println( new DataInputStream(fin).readLine() );
-		    fin.close();		
-		}
-
-		catch (IOException e){
-			System.err.println ("Unable to read from file");
-			System.exit(-1);
-		}
-	}
+	public static byte[] readTorrent(String torrentFile) {
+    StringBuffer buffer = new StringBuffer();
+    try {
+        FileInputStream fis = new FileInputStream(torrentFile);
+        InputStreamReader isr = new InputStreamReader(fis, "UTF8");
+        Reader in = new BufferedReader(isr);
+        int ch;
+        while ((ch = in.read()) > -1) {
+                buffer.append((char)ch);
+        }
+        in.close();
+        return buffer.toString().getBytes();
+    } catch (IOException e) {
+        e.printStackTrace();
+        return null;
+    }
+  }
 }
 
