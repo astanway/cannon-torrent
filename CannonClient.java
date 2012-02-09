cef6be95e4768e0d0cb965d8f1dd14a419e3058a
Abe Stanway <abestanway@gmail.com>
2012-02-09 12:17:49 -0500

    escaped url

--- CannonClient.java
+++ CannonClient.java
@@ -15,14 +15,32 @@ public class CannonClient {
 
     try{
       TorrentInfo decoded = new TorrentInfo(torrentArray);
+      byte[] escaped = new byte[20];
+      decoded.info_hash.get(escaped, 0, escaped.length);
+      String url = toURLHex(escaped);
+      //get list of peers from this decoded response
+      
       byte[] response = getURL(decoded.announce_url.toString());
       Object decoded_response = Bencoder2.decode(response);
-      //get list of peers from this decoded response
       
     } catch (Exception e){
       System.out.println(e);
     }
 	}
+	
+	public static final char[] HEX_CHARS = 
+   {'0','1','2','3','4','5','6','7',
+    '8','9','A','B','C','D','E','F'};
+
+  public static String toURLHex(byte[] bytes){
+     StringBuffer sb = new StringBuffer();
+     for(int i = 0; i < bytes.length; ++i){
+      sb.append('%')
+       .append(HEX_CHARS[(bytes[i]>>4&0x0F)])
+       .append(HEX_CHARS[(bytes[i]&0x0F)]);
+     }
+     return sb.toString();
+  }
 	
 	public static byte[] readTorrent(String torrentFile) {
     StringBuffer buffer = new StringBuffer();
