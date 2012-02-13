6c9c25860c0377b70618c7f3a8be576d22831ee4
Abe Stanway <abestanway@gmail.com>
2012-02-13 14:22:58 -0500

    fix bencoding exception

--- CannonClient.java
+++ CannonClient.java
@@ -132,19 +132,13 @@ public class CannonClient {
     return url_string;
 	}
 
-	
+	//read the torrent file into a byte array
 	public static byte[] readTorrent(String torrentFile) {
-    StringBuffer buffer = new StringBuffer();
     try {
-        FileInputStream fis = new FileInputStream(torrentFile);
-        InputStreamReader isr = new InputStreamReader(fis);
-        Reader in = new BufferedReader(isr);
-        int ch;
-        while ((ch = in.read()) > -1) {
-                buffer.append((char)ch);
-        }
-        in.close();
-        return buffer.toString().getBytes();
+      RandomAccessFile rFile = new RandomAccessFile(torrentFile,"rw");
+      byte[] fileBytes = new byte[(int)rFile.length()];
+      rFile.read(fileBytes);
+      return fileBytes;
     } catch (IOException e) {
         e.printStackTrace();
         return null;
